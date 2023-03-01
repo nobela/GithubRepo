06617ceb20de53463f68e20f54b97fd7ad121c19
ephraim <ephraim@DESKTOP-GSDR7ER>
2023-03-01 20:35:07 +0200

    Created class Employee

---
+++ src/githutest/Employee.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githutest;
+
+/**
+ *
+ * @author ephraim
+ */
+public class Employee {
+    private String ename;
+    private String sname;
+}
